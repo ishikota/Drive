9ad45afa4c85de385d09ef2b80c5a061e8a8e5f6
Kota Ishimoto <ishikota086@gmail.com>
2015-11-18 18:50:32 +0900

    Add BaseActivity and material style and color resource

---
+++ app/src/main/java/jp/ikota/drive/BaseActivity.java
@@ -0,0 +1,37 @@
+package jp.ikota.drive;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected Toolbar mActionBarToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((AndroidApplication)getApplication())
+                .getObjectGraph()
+                .inject(getApplication());
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        getActionBarToolbar();
+    }
+
+    protected Toolbar getActionBarToolbar() {
+        if (mActionBarToolbar == null) {
+            mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+            if (mActionBarToolbar != null) {
+                mActionBarToolbar.setTitle("");
+                mActionBarToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+                setSupportActionBar(mActionBarToolbar);
+            }
+        }
+        return mActionBarToolbar;
+    }
+}
